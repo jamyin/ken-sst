653fdb9334421d807ca8ed40c91a7b40e430cc77
mark <mark_chu@ssic.cn>
2016-03-16 17:10:36 +0800

    新增menu

---
+++ sst/sst-admin-api/src/main/java/com/tianfang/admin/service/IHomeMenuService.java
@@ -0,0 +1,30 @@
+package com.tianfang.admin.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.tianfang.admin.dto.HomeMenuDto;
+import com.tianfang.common.model.PageQuery;
+import com.tianfang.common.model.PageResult;
+
+@Service
+public interface IHomeMenuService {
+	/*public List<MenuDto> findSportHomeMenuList(HomeMenuDto dto);*/
+	
+	public PageResult<HomeMenuDto> findPage(HomeMenuDto dto,PageQuery page);
+	
+	public List<HomeMenuDto> findByMenuType(Integer menuType);
+	
+	public Integer save(HomeMenuDto HomeMenuDto) ;
+	
+	public HomeMenuDto findById(String id);
+	
+	public Integer edit(HomeMenuDto dto);
+	
+	public Integer delete(String ids);
+	
+	public List<HomeMenuDto> findByParentId(String parentId);
+	
+	public List<HomeMenuDto> findAll();
+}
