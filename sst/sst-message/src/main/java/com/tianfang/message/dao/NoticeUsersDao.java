2edad3414f39d576325eece1bb428400e10cfab4
wangxiang <xiang_wang@ssic.cn>
2016-03-21 17:48:38 +0800

    提供移动端.轮询获取最新一条公告

--- sst/sst-message/src/main/java/com/tianfang/message/dao/NoticeUsersDao.java
+++ sst/sst-message/src/main/java/com/tianfang/message/dao/NoticeUsersDao.java
@@ -1,17 +1,16 @@
 package com.tianfang.message.dao;
 
-import java.util.List;
-
-import lombok.Getter;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
 import com.tianfang.common.mybatis.MyBatisBaseDao;
+import com.tianfang.message.dto.NoticeDto;
 import com.tianfang.message.dto.NoticeUsersDto;
 import com.tianfang.message.mapper.NoticeUsersExMapper;
 import com.tianfang.message.mapper.NoticeUsersMapper;
 import com.tianfang.message.pojo.NoticeUsers;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class NoticeUsersDao extends MyBatisBaseDao<NoticeUsers> {
@@ -33,5 +32,7 @@ public class NoticeUsersDao extends MyBatisBaseDao<NoticeUsers> {
 		return mappers.releaseNotice(list);
 	}
 
-	
+	public NoticeDto getLast(String userId){
+		return mappers.getLast(userId);
+	}
 }
