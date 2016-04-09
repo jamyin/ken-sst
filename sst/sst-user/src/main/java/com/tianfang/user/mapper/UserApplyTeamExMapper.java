6e27e316211befa0e74635b3147cf4b4e3d655db
wangxiang <574693894@qq.com>
2016-04-09 16:23:52 +0800

    提交用户申请球队详情查询接口,修改球队管理员审核接口,修改球队管理员查询用户申请接口

--- sst/sst-user/src/main/java/com/tianfang/user/mapper/UserApplyTeamExMapper.java
+++ sst/sst-user/src/main/java/com/tianfang/user/mapper/UserApplyTeamExMapper.java
@@ -2,6 +2,7 @@ package com.tianfang.user.mapper;
 
 import java.util.List;
 
+import com.tianfang.user.app.AppUserInfo;
 import org.apache.ibatis.annotations.Param;
 
 import com.tianfang.common.model.PageQuery;
@@ -12,4 +13,8 @@ public interface UserApplyTeamExMapper {
 	int countByExample(@Param("example")UserApplyTeamDto example);
 
     List<UserApplyTeamDto> selectByExample(@Param("example")UserApplyTeamDto example, @Param("page") PageQuery page);
+
+    List<AppUserInfo> queryUserApplyInfoByParam(@Param("example")AppUserInfo example, @Param("page") PageQuery page);
+
+    int countUserApplyInfoByParam(@Param("example")AppUserInfo example);
 }
