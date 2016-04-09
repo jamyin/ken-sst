6b9ff80ac5af16afe1788c87561cc96e03dbb4e3
pengpeng_zhang <pengpeng_zhang@ssic.cn>
2016-04-09 12:33:22 +0800

    比赛几人制不能保存

--- sst/sst-message/src/main/java/com/tianfang/message/service/NoticeServiceImpl.java
+++ sst/sst-message/src/main/java/com/tianfang/message/service/NoticeServiceImpl.java
@@ -106,16 +106,16 @@ public class NoticeServiceImpl implements INoticeService {
 		}
 		return new PageResult<NoticeDto>(page, dtoList);*/
 		List<NoticeDto> list = noticeDao.findNoticeViewByPage(noticeDto ,  page);
-		if(list != null  && list.size() >0){
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd"); 
-			for(NoticeDto dto : list){
-				if(dto.getCreateTime() != null){
-					dto.setCreateTimeStr(sdf.format(dto.getCreateTime()));}
-				if(dto.getLastUpdateTime() != null){
-					dto.setLastUpdateTimeStr(sdf.format(dto.getLastUpdateTime()));
-				}
-			}
-		}
+//		if(list != null  && list.size() >0){
+//			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd"); 
+//			for(NoticeDto dto : list){
+//				if(dto.getCreateTime() != null){
+//					dto.setCreateTimeStr(sdf.format(dto.getCreateTime()));}
+//				if(dto.getLastUpdateTime() != null){
+//					dto.setLastUpdateTimeStr(sdf.format(dto.getLastUpdateTime()));
+//				}
+//			}
+//		}
 		int total = noticeDao.selectAccount(noticeDto);
 		page.setTotal(total);
 		return new PageResult<NoticeDto>(page, list);
