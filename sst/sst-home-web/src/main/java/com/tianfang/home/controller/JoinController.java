29500b475865f56dc2bf92ae1864de46fbecbf29
mark <mark_chu@ssic.cn>
2016-04-15 13:28:39 +0800

    文本信息

--- sst/sst-home-web/src/main/java/com/tianfang/home/controller/JoinController.java
+++ sst/sst-home-web/src/main/java/com/tianfang/home/controller/JoinController.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * 
+	 * 此类描述的是：申请加入球队的接口
+	 * @author: cwftalus@163.com
+	 * @version: 2016年4月15日 下午1:16:39
+ */
 @Controller
 public class JoinController extends BaseController{
 
