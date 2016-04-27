2d69f8aa42a8cec330c53abbc5d00bb2ac6aa7f4
mark <mark_chu@ssic.cn>
2016-04-27 13:47:05 +0800

    查看拒绝理由 接口

---
+++ sst/sst-home-web/src/main/java/com/tianfang/home/controller/CompReasonController.java
@@ -0,0 +1,27 @@
+package com.tianfang.home.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.tianfang.train.dto.CompetitionApplyDto;
+import com.tianfang.train.service.ICompetitionApplyService;
+
+@Controller
+public class CompReasonController extends BaseController{
+
+	@Autowired
+	private ICompetitionApplyService iCompetitionApplyService; 
+	
+	@RequestMapping(value = "comp/reason")
+	public ModelAndView  reason(String id) {
+		ModelAndView mv = new ModelAndView();
+
+		CompetitionApplyDto applyDto = iCompetitionApplyService.getCompetitionApplyById(id);
+
+		mv.addObject("applyDto", applyDto);
+		mv.setViewName("reason");
+		return mv;
+	}
+}
